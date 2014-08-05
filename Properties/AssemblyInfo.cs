b419f9124799f849e7abb59a3e9e5e235d148402
Karl Harnagy <kharnagy@inedo.com>
2014-08-05 17:01:43 -0400

    Fixed extension metadata.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,15 +3,15 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using Inedo.BuildMaster.Extensibility;
 
-[assembly: AssemblyTitle("TFS2012")]
-[assembly: AssemblyDescription("Source Control and Issue Tracking integration for Team Foundation Server 2012 and earlier.")]
+[assembly: AssemblyTitle("TFS")]
+[assembly: AssemblyDescription("Source Control and Issue Tracking integration for Team Foundation Server.")]
 
 [assembly: ComVisible(false)]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("BuildMaster")]
-[assembly: AssemblyCopyright("Copyright © 2008 - 2012")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2014")]
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0")]
 [assembly: BuildMasterAssembly]
 [assembly: CLSCompliant(false)]
-[assembly: RequiredBuildMasterVersion("3.0.0")]
+[assembly: RequiredBuildMasterVersion("4.4.0")]
