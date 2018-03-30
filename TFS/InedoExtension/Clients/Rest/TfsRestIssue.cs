936b8e9a4d4d2c5e1d5fb8fc6885e1e33eba95f3
Ben Lubar <ben.lubar@gmail.com>
2018-03-29 22:48:05 -0500

    Add closed states property to TfsIssueSource.
    Closes #22.

--- TFS/InedoExtension/Clients/Rest/TfsRestIssue.cs
+++ TFS/InedoExtension/Clients/Rest/TfsRestIssue.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Inedo.Extensibility.IssueSources;
 using Inedo.Extensions.TFS.VisualStudioOnline.Model;
 
@@ -6,13 +8,13 @@ namespace Inedo.Extensions.TFS.Clients.Rest
 {
     public sealed class TfsRestIssue : IIssueTrackerIssue
     {
-        public TfsRestIssue(GetWorkItemResponse w)
+        public TfsRestIssue(GetWorkItemResponse w, IEnumerable<string> closedStates)
         {
             this.Id = w.id.ToString();
             this.Title = w.fields.GetValueOrDefault("System.Title")?.ToString();
             this.Description = w.fields.GetValueOrDefault("System.Description")?.ToString();
             this.Status = w.fields.GetValueOrDefault("System.State")?.ToString();
-            this.IsClosed = "Done".Equals(this.Status, StringComparison.OrdinalIgnoreCase);
+            this.IsClosed = closedStates.Contains(this.Status, StringComparer.OrdinalIgnoreCase);
             this.SubmittedDate = AH.ParseDate(w.fields.GetValueOrDefault("System.CreatedDate")?.ToString()) ?? DateTime.MinValue;
             this.Submitter = w.fields.GetValueOrDefault("System.CreatedBy")?.ToString();
             this.Type = w.fields.GetValueOrDefault("System.WorkItemType")?.ToString();
