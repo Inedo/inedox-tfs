3503505244ed2167c32e3236d05e81cf2f583166
Richard Hessinger <rhessinger@inedo.com>
2023-02-20 14:29:02 -0500

    Fixed extension naming

--- TFS/InedoExtension/Properties/AssemblyInfo.cs
+++ TFS/InedoExtension/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using Inedo.Extensibility;
 
 [assembly: AssemblyTitle("TFS")]
-[assembly: AssemblyDescription("Source Control and Issue Tracking integration for Team Foundation Server and Azure DevOps (formerly Visual Studio Team Services).")]
+[assembly: AssemblyDescription("TFVC Source Control for Team Foundation Server and Azure DevOps (formerly Visual Studio Team Services).")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("any")]
 [assembly: AssemblyCopyright("Copyright © Inedo 2023")]
